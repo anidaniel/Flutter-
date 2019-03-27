16cbf69cbc7aecc9359fc196fb3f7f551af7e191
anidaniel <anidanielcorp@gmail.com>
2019-03-27 17:48:54 +0530

    StatefulWidget_compatv1.2

--- lib/main.dart
+++ lib/main.dart
@@ -6,33 +6,41 @@ main() {
 
 //main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Anidaniel List'),
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('Add Product'),
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('Sushi - JAPAN')
-                  ],
-                ),
-              ),
-            ],
-          )),
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
   }
 }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           appBar: AppBar(
+//             title: Text('Anidaniel List'),
+//           ),
+//           body: Column(
+//             children: [
+//               Container(
+//                 margin: EdgeInsets.all(10.0),
+//                 child: RaisedButton(
+//                   onPressed: () {
+
+//                   },
+//                   child: Text('Add Product'),
+//                 ),
+//               ),
+//               Card(
+//                 child: Column(
+//                   children: <Widget>[
+//                     Image.asset('assets/food.jpg'),
+//                     Text('Sushi - JAPAN')
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           )),
+//     );
+//   }
+// }
